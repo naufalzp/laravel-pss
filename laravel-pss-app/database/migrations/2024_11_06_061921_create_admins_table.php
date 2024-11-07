48b705951bd986ff47bfd550395cf21b49dc61ce
naufalzp <naufalarya0@gmail.com>
2024-11-07 14:29:21 +0700

    refactor: Make email field nullable in admins table migration

--- laravel-pss-app/database/migrations/2024_11_06_061921_create_admins_table.php
+++ laravel-pss-app/database/migrations/2024_11_06_061921_create_admins_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('username', 50)->unique();
             $table->string('password', 100);
-            $table->string('email', 100)->unique();
+            $table->string('email', 100)->unique()->nullable();
             $table->timestamps();
         });
     }
