fc4566521f0c1920a571de837cad95374552cbe4
aurelwidyanti <aurelwyt@gmail.com>
2024-11-07 13:10:08 +0700

    feat: Add Supplier routes for CRUD operations in API

--- laravel-pss-app/routes/api.php
+++ laravel-pss-app/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\AdminController;
+use App\Http\Controllers\API\SupplierController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,3 +10,9 @@ Route::post('/admins', [AdminController::class, 'store']);
 Route::get('/admins/{id}', [AdminController::class, 'show']);
 Route::put('/admins/{id}', [AdminController::class, 'update']);
 Route::delete('/admins/{id}', [AdminController::class, 'destroy']);
+
+Route::get('/suppliers', [SupplierController::class, 'index']);
+Route::post('/suppliers', [SupplierController::class, 'store']);
+Route::get('/suppliers/{id}', [SupplierController::class, 'show']);
+Route::put('/suppliers/{id}', [SupplierController::class, 'update']);
+Route::delete('/suppliers/{id}', [SupplierController::class, 'destroy']);
