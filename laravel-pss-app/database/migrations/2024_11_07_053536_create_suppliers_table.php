40e23e140f2a281235b21987fd129f4f88d380a5
aurelwidyanti <aurelwyt@gmail.com>
2024-11-08 14:29:09 +0700

    refactor: Change created_by column type to unsignedInteger in suppliers table migration

--- laravel-pss-app/database/migrations/2024_11_07_053536_create_suppliers_table.php
+++ laravel-pss-app/database/migrations/2024_11_07_053536_create_suppliers_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name', 100);
             $table->string('contact_info', 100)->nullable();
-            $table->unsignedBigInteger('created_by');
+            $table->unsignedInteger('created_by');
             $table->foreign('created_by')->references('id')->on('admins');
             $table->timestamps();
         });
