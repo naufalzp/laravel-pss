e8516f0e3a00cc733092eecde2182165cbba6813
naufalzp <naufalarya0@gmail.com>
2024-11-11 08:26:56 +0700

    feat: implement Admin model for authentication

--- laravel-pss-app/app/Models/Admin.php
+++ laravel-pss-app/app/Models/Admin.php
@@ -2,18 +2,38 @@
 
 namespace App\Models;
 
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Models\Contracts\HasName;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Admin extends Model
+class Admin extends Authenticatable implements FilamentUser, HasName
 {
+     /** @use HasFactory<\Database\Factories\UserFactory> */
+     use HasFactory, Notifiable, HasApiTokens;
+
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
     protected $fillable = [
         'username',
         'password',
         'email',
     ];
 
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
     protected $hidden = [
         'password',
+        'remember_token',
     ];
 
     public function categories()
@@ -29,5 +49,15 @@ class Admin extends Model
     public function items()
     {
         return $this->hasMany(Item::class,'created_by');
+    }
+
+    public function canAccessPanel($panel): bool
+    {
+        return true;
+    }
+
+    public function getFilamentName(): string
+    {
+        return $this->username;
     }
 }
