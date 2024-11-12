fca9f33c7080b26602978839773b098e296bb824
naufalzp <naufalarya0@gmail.com>
2024-11-12 19:41:31 +0700

    fix: add cascade on delete to foreign key for created_by in categories table

--- laravel-pss-app/database/migrations/2024_11_06_160119_create_categories_table.php
+++ laravel-pss-app/database/migrations/2024_11_06_160119_create_categories_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name', 100);
             $table->text('description')->nullable();
             $table->unsignedBigInteger('created_by');
-            $table->foreign('created_by')->references('id')->on('admins');
+            $table->foreign('created_by')->references('id')->on('admins')->cascadeOnDelete();
             $table->timestamps();
         });
     }
